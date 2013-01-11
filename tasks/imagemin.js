236976d69b34e4bbd965b8780cbb15a5d90a846b
J. Tangelder <j.tangelder@gmail.com>
2013-01-11 09:16:22 +0100

    fixed indents and cleanup

--- tasks/imagemin.js
+++ tasks/imagemin.js
@@ -9,78 +9,77 @@
 'use strict';
 
 module.exports = function (grunt) {
-    var path = require('path');
-    var fs = require('fs');
-    var childProcess = require('child_process');
-    var filesize = require('filesize');
-    var optipngPath = require('optipng-bin').path;
-    var jpegtranPath = require('jpegtran-bin').path;
+  var path = require('path');
+  var fs = require('fs');
+  var childProcess = require('child_process');
+  var filesize = require('filesize');
+  var optipngPath = require('optipng-bin').path;
+  var jpegtranPath = require('jpegtran-bin').path;
 
-    grunt.registerMultiTask('imagemin', 'Minify PNG and JPEG images', function () {
-        var cb = this.async();
-        var options = this.options();
+  grunt.registerMultiTask('imagemin', 'Minify PNG and JPEG images', function () {
+    var options = this.options();
 
-        var optipngArgs = ['-strip', 'all', '-clobber'];
-        var jpegtranArgs = ['-copy', 'none', '-optimize'];
+    var optipngArgs = ['-strip', 'all', '-clobber'];
+    var jpegtranArgs = ['-copy', 'none', '-optimize'];
 
-        if (typeof options.optimizationLevel === 'number') {
-            optipngArgs.push('-o', options.optimizationLevel);
+    if (typeof options.optimizationLevel === 'number') {
+      optipngArgs.push('-o', options.optimizationLevel);
+    }
+
+    if (typeof options.progressive === 'number') {
+      jpegtranArgs.push('-progressive');
+    }
+
+    grunt.verbose.writeflags(options, 'Options');
+
+    grunt.util.async.forEach(this.files, function (files, next) {
+      files.src.forEach(function (src) {
+        var dest = files.dest;
+        if (path.extname(dest) === '') {
+          dest = path.join(dest, path.basename(src));
+        }
+        optimize(src, dest, next);
+      });
+    }.bind(this), this.async());
+
+
+    function optimize(src, dest, next) {
+
+
+      function processed(err, result, code) {
+        var saved, savedMsg;
+
+        if (err) {
+          grunt.log.writeln(err);
         }
 
-        if (typeof options.progressive === 'number') {
-            jpegtranArgs.push('-progressive');
+        grunt.file.mkdir(path.dirname(dest));
+
+        saved = fs.statSync(src).size - fs.statSync(dest).size;
+
+        if (result.stderr.indexOf('already optimized') !== -1 || saved < 10) {
+          savedMsg = 'already optimized';
+        } else {
+          savedMsg = 'saved ' + filesize(saved);
         }
 
-        grunt.verbose.writeflags(options, 'Options');
+        grunt.log.writeln('✔ '.green + src + (' (' + savedMsg + ')').grey);
+        next();
+      }
 
-        grunt.util.async.forEach(this.files, function(files, next) {
-            files.src.forEach(function(src) {
-                var dest = files.dest;
-                if(path.extname(dest) === '') {
-                    dest = path.join(dest, path.basename(src));
-                }
-                optimize(src, dest, next);
-            });
-        }.bind(this), this.async());
-
-
-        function optimize(src, dest, next) {
-
-
-            function processed(err, result, code) {
-                var saved, savedMsg;
-
-                if (err) {
-                    grunt.log.writeln(err);
-                }
-
-                grunt.file.mkdir(path.dirname(dest));
-
-                saved = fs.statSync(src).size - fs.statSync(dest).size;
-
-                if (result.stderr.indexOf('already optimized') !== -1 || saved < 10) {
-                    savedMsg = 'already optimized';
-                } else {
-                    savedMsg = 'saved ' + filesize(saved);
-                }
-
-                grunt.log.writeln('✔ '.green + src + (' (' + savedMsg + ')').grey);
-                next();
-            }
-
-            if (path.extname(src) === '.png') {
-                grunt.util.spawn({
-                    cmd:optipngPath,
-                    args:optipngArgs.concat(['-out', dest, src])
-                }, processed);
-            } else if (['.jpg', '.jpeg'].indexOf(path.extname(src)) !== -1) {
-                grunt.util.spawn({
-                    cmd:jpegtranPath,
-                    args:jpegtranArgs.concat(['-outfile', dest, src])
-                }, processed);
-            } else {
-                next();
-            }
-        }
-    });
+      if (path.extname(src) === '.png') {
+        grunt.util.spawn({
+          cmd:optipngPath,
+          args:optipngArgs.concat(['-out', dest, src])
+        }, processed);
+      } else if (['.jpg', '.jpeg'].indexOf(path.extname(src)) !== -1) {
+        grunt.util.spawn({
+          cmd:jpegtranPath,
+          args:jpegtranArgs.concat(['-outfile', dest, src])
+        }, processed);
+      } else {
+        next();
+      }
+    }
+  });
 };
