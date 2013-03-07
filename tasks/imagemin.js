e24a354cc1370e4f2c9d06dabce7393804a67b3c
Crow Norlander <cnorlander@undertone.com>
2013-03-07 17:20:04 -0500

    limit to 30

--- tasks/imagemin.js
+++ tasks/imagemin.js
@@ -31,7 +31,7 @@ module.exports = function (grunt) {
 
         grunt.verbose.writeflags(options, 'Options');
 
-        grunt.util.async.forEachLimit(this.files, 10, function (files, next) {
+        grunt.util.async.forEachLimit(this.files, 30, function (files, next) {
             files.src.forEach(function (src) {
                 var dest = files.dest;
                 if (path.extname(dest) === '') {
