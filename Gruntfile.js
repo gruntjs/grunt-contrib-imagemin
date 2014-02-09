8545a942b0459e075cd46dae661b2cadabf93408
clement <maboiteaspam@wanadoo.fr>
2014-02-09 17:11:32 +0800

    Added event driven demo

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,57 +9,57 @@
 'use strict';
 
 module.exports = function (grunt) {
-    require('time-grunt')(grunt);
+  require('time-grunt')(grunt);
 
-    grunt.initConfig({
-        jshint: {
-            all: [
-                'Gruntfile.js',
-                'tasks/*.js',
-                '<%= nodeunit.tests %>'
-            ],
-            options: {
-                jshintrc: '.jshintrc'
-            }
-        },
-        clean: {
-            test: ['tmp']
-        },
-        imagemin: {
-            dist: {
-                files: {
-                    'tmp/test.png': 'test/fixtures/test.png',
-                    'tmp/test.jpg': 'test/fixtures/test.jpg',
-                    'tmp/test.gif': 'test/fixtures/test.gif',
-                    'tmp/test-uppercase.PNG': 'test/fixtures/test-uppercase.PNG',
-                    'tmp/test-uppercase.JPG': 'test/fixtures/test-uppercase.JPG',
-                    'tmp/test-uppercase.GIF': 'test/fixtures/test-uppercase.GIF'
-                }
-            }
-        },
-        nodeunit: {
-            tests: ['test/test.js']
+  grunt.initConfig({
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'tasks/*.js',
+        '<%= nodeunit.tests %>'
+      ],
+      options: {
+        jshintrc: '.jshintrc'
+      }
+    },
+    clean: {
+      test: ['tmp']
+    },
+    imagemin: {
+      dist: {
+        files: {
+          'tmp/test.png': 'test/fixtures/test.png',
+          'tmp/test.jpg': 'test/fixtures/test.jpg',
+          'tmp/test.gif': 'test/fixtures/test.gif',
+          'tmp/test-uppercase.PNG': 'test/fixtures/test-uppercase.PNG',
+          'tmp/test-uppercase.JPG': 'test/fixtures/test-uppercase.JPG',
+          'tmp/test-uppercase.GIF': 'test/fixtures/test-uppercase.GIF'
         }
-    });
+      }
+    },
+    nodeunit: {
+      tests: ['test/test.js']
+    }
+  });
 
-    grunt.loadTasks('tasks');
+  grunt.loadTasks('tasks');
 
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-nodeunit');
-    grunt.loadNpmTasks('grunt-contrib-internal');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  grunt.loadNpmTasks('grunt-contrib-internal');
 
-    grunt.registerTask('mkdir', grunt.file.mkdir);
+  grunt.registerTask('mkdir', grunt.file.mkdir);
 
-    grunt.registerTask('test', [
-        'clean',
-        'mkdir:tmp',
-        'imagemin',
-        'nodeunit',
-        'clean'
-    ]);
+  grunt.registerTask('test', [
+    'clean',
+    'mkdir:tmp',
+    'imagemin',
+    'nodeunit',
+    'clean'
+  ]);
 
-    grunt.registerTask('default', ['test', 'build-contrib']);
+  grunt.registerTask('default', ['test', 'build-contrib']);
 
 
     // event driven logging demonstration
