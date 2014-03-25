e4de60127318b5e45392af020e1b9d4d0a487e0c
Ramon Fritsch <ramon@joy-interactive.com>
2014-03-25 01:24:14 -0300

    checking for folder existence
    Creating subfolders if needed

--- tasks/imagemin.js
+++ tasks/imagemin.js
@@ -15,6 +15,39 @@ var imagemin = require('image-min');
  * Licensed under the MIT license.
  */
 
+function mkdir_p(dir, callback, position)
+{
+    position = position || 0;
+    var parts = path.normalize(dir).split('/');
+ 
+    if (position >= parts.length) {
+        if (callback) {
+            return callback();
+        } else {
+            return true;
+        }
+    }
+ 
+    var directory = parts.slice(0, position + 1).join('/');
+    fs.stat(directory, function(err) {
+        if (err === null) {
+            mkdir_p(dir, callback, position + 1);
+        } else {
+            fs.mkdir(directory, function (err) {
+                // if (err) {
+                //     if (callback) {
+                //         return callback(err);
+                //     } else {
+                //         throw err;
+                //     }
+                // } else {
+                    mkdir_p(dir, callback, position + 1);
+                // }
+            })
+        }
+    })
+}
+
 module.exports = function (grunt) {
     grunt.registerMultiTask('imagemin', 'Minify PNG, JPEG and GIF images', function () {
         var done = this.async();
@@ -28,23 +61,15 @@ module.exports = function (grunt) {
 
         async.forEachLimit(this.files, os.cpus().length, function (file, next) {
             options.ext = path.extname(file.src[0]);
+            var dir = path.dirname(file.dest);
 
             if (!options.ext)
             {
-                fs.mkdir(file.dest, function (err) {
-                    if (err)
-                    {
-                        return grunt.warn(err);
-                    }
-
-                    grunt.log.writeln(chalk.green('✔ ') + file.src[0] + chalk.gray(' -> ' + file.dest));
-                    return next();
-                });
-
-                return;
+                return next();
             }
 
-            fs.createReadStream(file.src[0])
+            var run = function () {
+                fs.createReadStream(file.src[0])
                 .pipe(imagemin(options)
                     .on('error', function (err) {
                         grunt.warn(err);
@@ -66,6 +91,26 @@ module.exports = function (grunt) {
                         grunt.log.writeln(chalk.green('✔ ') + file.src[0] + chalk.gray(' (' + msg + ')'));
                         next();
                     }));
+            };
+
+            fs.stat(dir, function (err, stats) {
+                
+                if (err || !stats.isDirectory())
+                {
+                    mkdir_p(dir, function (err) {
+                        if (err)
+                        {
+                            return grunt.warn(err);
+                        }
+
+                        run();
+                    });
+                }
+                else
+                {
+                    run();
+                }
+            });
         }, function (err) {
             if (err) {
                 grunt.warn(err);
