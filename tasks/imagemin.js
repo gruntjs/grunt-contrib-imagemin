411c98daa2e7e18b72c74fb13112567dff347d48
Sergei Prognimak <sergei@prognimak.com>
2015-11-08 19:57:25 +0200

    Workaround for Cannot read property 'contents' of undefined

--- tasks/imagemin.js
+++ tasks/imagemin.js
@@ -58,7 +58,7 @@ module.exports = function (grunt) {
                     }
 
                     var origSize = stats.size;
-                    var diffSize = origSize - data[0].contents.length;
+                    var diffSize = data.length ? (origSize - data[0].contents.length) : 0;
 
                     totalSaved += diffSize;
 
